2157eac48e5567ca898df38caaa982eae2f5d16f
Tomáš Votava <tomas.votava@bizztreat.com>
2018-12-27 11:01:34 +0100

    42

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
+#!/usr/bin/env python3
 
-print("Hello world from python")
+import requests
+import csv
+import json
+import os
+
+with open("/data/config.json","r") as conf_file:
+    conf = json.load(conf_file)["parameters"]
+
+endpoint = "https://{0}.talon.one/v1/applications/{1}/campaigns/{2}/import_coupons".format(conf["project"], conf["application-id"], conf["campaign-id"])
+headers = {"authorization":"Bearer {0}".format(conf["#bearer"])}
+
+if not os.path.exists("/data/in/tables/input.csv"):
+    raise Exception("Input table must be named \"input.csv\"")
+
+files = {'file': open("/data/in/tables/input.csv", "rb")}
+
+r1 = requests.post(endpoint,headers=headers,files=files)
